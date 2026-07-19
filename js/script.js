137b24eb38285a68a525148afb3176f24f456c75
3ZOZTRADING <3zoztrading2020@gmail.com>
2026-07-19 05:54:32 +0300

    JavaScript Part 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
+/* ===================================
+   3ZOZ Trading
+   JavaScript Part 1
+=================================== */
 
+
+// Page Loaded
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+console.log("3ZOZ Trading Website Loaded");
+
+
+
+/* Header Scroll Effect */
+
+
+const header = document.querySelector("header");
+
+
+window.addEventListener("scroll",()=>{
+
+
+    if(window.scrollY > 50){
+
+        header.style.background = "rgba(255,255,255,.98)";
+
+    }else{
+
+        header.style.background = "rgba(255,255,255,.95)";
+
+    }
+
+
+});
+
+
+
+});
